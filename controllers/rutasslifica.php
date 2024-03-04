37975d2f82601f07bbccb4e45db8625008d9e4c6
Kappo <andrea30.4.99@gmail.com>
2024-03-05 00:46:48 +0100

    Update rutasslifica.php

--- controllers/rutasslifica.php
+++ controllers/rutasslifica.php
@@ -8,7 +8,7 @@ $app->group('/rutasslifica', function ($group) {
     // GET /rutasslifica
     $group->get('', function (Request $request, Response $response, $args) {
         $rutasslifica = Rutasslifica::getRutasslifica();
-            $httpResponse = new HttpResponse(Status::NotFound, "Not Found rutasslifica");
+            $httpResponse = new HttpResponse(Status::NotFound, "Not Found rutasslifica", $rutasslifica);
 
         // // for ($i = 0; $i < count($rutasslifica); $i++) {
         // //     $rutasslifica[$i] = $rutasslifica[$i]->toArray();
