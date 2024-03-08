5018df68c4876683400ac1ef9b70a569c5929cdf
Kappo <andrea30.4.99@gmail.com>
2024-03-08 11:52:20 +0100

    Update model.php

--- include/model.php
+++ include/model.php
@@ -5,4 +5,4 @@ require_once SERVER_ROOT . 'model/Rutatore.php';
 require_once SERVER_ROOT . 'model/Rutazione.php';
 require_once SERVER_ROOT . 'model/RutaBonus.php';
 require_once SERVER_ROOT . 'model/Rutasslifica.php';
-require_once SERVER_ROOT . 'model/Formazioni.php';
+require_once SERVER_ROOT . 'model/Formazione.php';
