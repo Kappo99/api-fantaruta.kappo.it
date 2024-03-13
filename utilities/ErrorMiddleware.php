ac16488f1e7596f4528d9654669518928766d239
Kappo <andrea30.4.99@gmail.com>
2024-03-13 15:23:27 +0100

    Update ErrorMiddleware.php

--- utilities/ErrorMiddleware.php
+++ utilities/ErrorMiddleware.php
@@ -19,11 +19,7 @@ class ErrorMiddleware
             $log = new Log(null, $method, $uri, $params, $state);
             $log_id = Log::addLog($log);
 
-            $response = $handler->handle($request)
-                        ->withHeader('Content-Type', 'application/json')
-                        ->withHeader('Access-Control-Allow-Origin', '*')
-                        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-                        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');;
+            $response = $handler->handle($request)->withHeader('Content-Type', 'application/json');
 
             // $error = $response->getStatusCode() != 200 ? $response->getBody()->getContents() : null;
             // TODO: Aggiornare il LOG con Error_Log se StatusCode !== 200
