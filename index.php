12ec32993ba765ecdd5668d0e4277fd696c0aeaa
Kappo <andrea30.4.99@gmail.com>
2024-03-13 15:25:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,17 +15,18 @@ $app = AppFactory::create();
 // Aggiungi il middleware di gestione degli errori
 $app->add(new ErrorMiddleware());
 
+//* Add CORS Middleware for preloader request
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
 
-// $app->add(function ($request, $handler) {
-//     $response = $handler->handle($request);
-//     return $response
-//             ->withHeader('Access-Control-Allow-Origin', '*')
-//             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-//             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-// });
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 // Includi le rotte per gli utenti
 require_once __DIR__ . '/include/controllers.php';
